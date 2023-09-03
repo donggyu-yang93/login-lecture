9d67935981f558b76c9c58b3350aa8a368e12083
Mr.Yang <ydg0614@gmail.com>
2023-09-03 21:33:11 +0900

    프론트

--- app/app.js
+++ app/app.js
@@ -26,6 +26,7 @@ const home = require("./src/routes/home");
 // 앱 세팅
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
+app.use(express.static(`${__dirname}/src/public/`)); // dir == 현재 app 파일이 있는 위치 반환 /src/public
 
 
 app.use("/", home); // use --> 미들웨어 등록 메서드.
